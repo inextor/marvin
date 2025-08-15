85e85454847c7a15a5b992c67dcf6bf50690a884
nextor <nextor@gmail.com>
2025-08-15 13:52:25 -0700

    Debug: Simplify background.js to test message reception

--- background.js
+++ background.js
@@ -1,45 +1,5 @@
 console.log('Background script loaded');
 
-let port;
-let currentMcpStatus = 'Disconnected';
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "start") {
-    if (!port) {
-      port = chrome.runtime.connectNative('com.my.native_host');
-
-      port.onMessage.addListener((msg) => {
-        if (msg.action === 'getBrowserContent') {
-          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            if (tabs[0]) {
-              chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: () => {
-                  const h1 = document.querySelector('h1');
-                  return h1 ? h1.textContent : '';
-                }
-              }, (results) => {
-                const h1Content = results[0].result;
-                port.postMessage({ action: 'response', id: msg.id, data: h1Content });
-              });
-            }
-          });
-        } else if (msg.action === 'mcpStatus') {
-          console.log('MCP Status:', msg.status);
-          currentMcpStatus = msg.status;
-          chrome.runtime.sendMessage({ action: 'mcpStatus', status: msg.status });
-        } else if (msg.action === 'response') {
-          chrome.runtime.sendMessage({ action: 'response', message: msg.text });
-        }
-      });
-
-      port.onDisconnect.addListener(() => {
-        port = null;
-        currentMcpStatus = 'Disconnected';
-        chrome.runtime.sendMessage({ action: 'mcpStatus', status: currentMcpStatus });
-      });
-    }
-  } else if (request.action === 'requestStatus') {
-    chrome.runtime.sendMessage({ action: 'mcpStatus', status: currentMcpStatus });
-  }
+  console.log('Message received in background script:', request);
 });
